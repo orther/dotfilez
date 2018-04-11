6519b05b3879b90d47c3fe6487b555996321f44a
Brandon Orther <brandon@uptrend.tech>
2018-04-11 13:07:38 -0700

    Update hlissner's dotfiles for personal use
    I am trying Arch Linux for first time by dual booting laptop. Figured
    I'd see how his dotfiles work as a starting point.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 # Installs zsh & git, then clones this repo to ~/.dotfiles. This script is
 # intended to be run remotely, via curl:
 #
-# bash <(curl -s https://raw.githubusercontent.com/hlissner/dotfiles/master/bootstrap.sh)
+# bash <(curl -s https://raw.githubusercontent.com/orther/dotfiles/master/bootstrap.sh)
 
 export DOTFILES=~/.dotfiles
 set -e
@@ -25,10 +25,10 @@ fi
 
 if [[ ! -d ~/.dotfiles ]]; then
   _msg "Deploying dotfiles repository..."
-  if [[ $USER == hlissner ]]; then
-    dfrepo=git@github.com:hlissner/dotfiles.git
+  if [[ $USER == orther ]]; then
+    dfrepo=git@github.com:orther/dotfilez.git
   else
-    dfrepo=https://github.com/hlissner/dotfiles
+    dfrepo=https://github.com/orther/dotfilez
   fi
   git clone --recursive "$dfrepo" "$DOTFILES"
 fi
